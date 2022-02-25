1ca59bc15d2015689e5932f29927367ad2763580
cafehaine <kilian.guillaume@gmail.com>
2022-02-25 11:06:42 +0100

    Server: Remove websockets, bump most versions

--- rts-server/src/main.rs
+++ rts-server/src/main.rs
@@ -1,8 +1,3 @@
-//! Simple echo websocket server.
-//! Open `http://localhost:8080/index.html` in browser
-//! or [python console client](https://github.com/actix/examples/blob/master/websocket/websocket-client.py)
-//! could be used for testing.
-
 #[macro_use]
 extern crate diesel;
 
@@ -11,15 +6,12 @@ pub mod schema;
 
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
-use std::time::{Duration, Instant};
 
-use actix::prelude::*;
 use actix_files as fs;
 use actix_web::http::Cookie;
 use actix_web::{
-    get, middleware, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+    get, middleware, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
-use actix_web_actors::ws;
 use argon2::{self, Config};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
@@ -37,11 +29,6 @@ use rts_core::entity::unit::UnitType;
 use self::models::*;
 use self::schema::*;
 
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-
 const AUTH_COOKIE_NAME: &str = "_token";
 
 pub type PostgresPool = Pool<ConnectionManager<PgConnection>>;
@@ -50,82 +37,6 @@ pub struct AppState<'a> {
     pool: PostgresPool,
     argon2_config: Config<'a>,
     tokens: Arc<RwLock<HashMap<String, i32>>>,
-}
-
-/// do websocket handshake and start `MyWebSocket` actor
-async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
-    let res = ws::start(MyWebSocket::new(), &r, stream);
-    println!("{:?}", res);
-    res
-}
-
-/// websocket connection is long running connection, it easier
-/// to handle with an actor
-struct MyWebSocket {
-    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    /// otherwise we drop connection.
-    hb: Instant,
-}
-
-impl Actor for MyWebSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    /// Method is called on actor start. We start the heartbeat process here.
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.hb(ctx);
-    }
-}
-
-/// Handler for `ws::Message`
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        // process websocket messages
-        println!("WS: {:?}", msg);
-        match msg {
-            Ok(ws::Message::Ping(msg)) => {
-                self.hb = Instant::now();
-                ctx.pong(&msg);
-            }
-            Ok(ws::Message::Pong(_)) => {
-                self.hb = Instant::now();
-            }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => {
-                ctx.close(reason);
-                ctx.stop();
-            }
-            _ => ctx.stop(),
-        }
-    }
-}
-
-impl MyWebSocket {
-    fn new() -> Self {
-        Self { hb: Instant::now() }
-    }
-
-    /// helper method that sends ping to client every second.
-    ///
-    /// also this method checks heartbeats from client
-    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
-            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
-                // heartbeat timed out
-                println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
-                ctx.stop();
-
-                // don't try to send a ping
-                return;
-            }
-
-            ctx.ping(b"");
-        });
-    }
 }
 
 #[derive(Deserialize)]
@@ -437,8 +348,6 @@ async fn main() -> std::io::Result<()> {
             .service(submit_ai)
             // leaderboard route
             .service(leaderboard)
-            // websocket route
-            .service(web::resource("/ws/").route(web::get().to(ws_index)))
             // default to files in ./rts-server/static
             .service(fs::Files::new("/", "./rts-server/static/").index_file("index.html"))
     })
