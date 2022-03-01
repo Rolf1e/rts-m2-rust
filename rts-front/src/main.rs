1864d11669524cc60f3f24a3a3e87300ab7b6c53
cafehaine <kilian.guillaume@gmail.com>
2022-03-01 10:11:12 +0100

    Front: Start implementing login page

--- rts-front/src/main.rs
+++ rts-front/src/main.rs
@@ -1,5 +1,7 @@
 use reqwasm::http::Request;
 use serde::Deserialize;
+use web_sys::HtmlInputElement;
+use weblog::console_log;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -27,8 +29,46 @@ fn home_screen() -> Html {
 
 #[function_component(Login)]
 fn login_screen() -> Html {
+    let username = use_state(|| "".to_string());
+    let password = use_state(|| "".to_string());
+
+    let on_username_change = {
+        let username = username.clone();
+        Callback::from(move |e: Event| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            username.set(input.value());
+        })
+    };
+
+    let on_password_change = {
+        let password = password.clone();
+        Callback::from(move |e: Event| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            password.set(input.value());
+        })
+    };
+
+    let on_submit = {
+        let username = username.clone();
+        let password = password.clone();
+        Callback::from(move |_| {
+            // TODO
+            console_log!(
+                "Username:",
+                (*username).clone(),
+                "Password:",
+                (*password).clone()
+            )
+        })
+    };
+
     html! {
-        <h1>{ "Login screen!" }</h1>
+        <>
+            <h1>{ "Login screen!" }</h1>
+            <label>{ "Username:" }<input onchange={on_username_change} value={(*username).clone()}/></label>
+            <label>{ "Password:" }<input onchange={on_password_change} value={(*password).clone()} type="password"/></label>
+            <button onclick={on_submit}>{ "Login" }</button>
+        </>
     }
 }
 
@@ -64,29 +104,37 @@ fn switch(routes: &Route) -> Html {
 }
 
 #[derive(Clone, PartialEq, Deserialize)]
-struct LoginState {
-    username: String,
-    user_id: i32,
+#[serde(untagged)]
+enum LoginState {
+    LoggedOut,
+    LoggedIn { username: String, user_id: i32 },
+    Checking,
 }
 
 #[function_component(App)]
 fn app() -> Html {
-    let login_state = use_state(|| None);
+    let login_state = use_state(|| LoginState::Checking);
     {
         let login_state = login_state.clone();
         use_effect_with_deps(
             move |_| {
                 let login_state = login_state.clone();
                 wasm_bindgen_futures::spawn_local(async move {
-                    let fetched_state: Option<LoginState> = match Request::get("/api/login_status")
+                    let fetched_state: LoginState = match Request::get("/api/login_status")
                         .send()
                         .await
                         .unwrap()
                         .json()
                         .await
                     {
-                        Ok(state) => Some(state),
-                        Err(_) => None,
+                        Ok(state) => state,
+                        Err(err) => {
+                            let window = gloo_utils::window();
+                            window
+                                .alert_with_message(&format!("Error checking login: {}", err))
+                                .unwrap();
+                            LoginState::LoggedOut
+                        }
                     };
                     login_state.set(fetched_state);
                 });
@@ -106,9 +154,10 @@ fn app() -> Html {
                         <li><Link<Route> to={Route::AIUpload}>{ "Upload AI" }</Link<Route>></li>
                         <li><a href="https://github.com/Rolf1e/rts-m2-rust">{ "View on github" }</a></li>
                         <li>{
-                            match *login_state {
-                                None => html! { <Link<Route> to={Route::Login}>{ "Log in" }</Link<Route>> },
-                                Some(_) => html! { <Link<Route> to={Route::HomeScreen}>{ "Log out" }</Link<Route>> },
+                            match (*login_state).clone() {
+                                LoginState::Checking => html! { "Loading…" },
+                                LoginState::LoggedOut => html! { <Link<Route> to={Route::Login}>{ "Log in" }</Link<Route>> },
+                                LoginState::LoggedIn{ username: _, user_id: _ } => html! { <Link<Route> to={Route::HomeScreen}>{ "Log out" }</Link<Route>> },
                             }
                         }</li>
                     </ul>
