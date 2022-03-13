27082a5073638370fc1666461c0928a598b12258
Rolf1e <tigran.slama@laposte.net>
2022-03-13 19:24:56 +0100

    [Server] update up.sql script

--- rts-server/migrations/2022-03-05-121711_create_leader_board/up.sql
+++ rts-server/migrations/2022-03-05-121711_create_leader_board/up.sql
@@ -8,4 +8,10 @@ CREATE TABLE MATCHS(
 );
 
 INSERT INTO users (username, password, email) VALUES ('rolfie', 'rolfie', '' );
+INSERT INTO users (username, password, email) VALUES ('tigran', 'tigran', '' );
+INSERT INTO users (username, password, email) VALUES ('antoine', 'antoine', '' );
 INSERT INTO matchs (winner, looser, score_winner, score_looser) VALUES (1, 1, 100, 99);
+INSERT INTO matchs (winner, looser, score_winner, score_looser) VALUES (2, 1, 120, 99);
+INSERT INTO matchs (winner, looser, score_winner, score_looser) VALUES (1, 2, 120, 99);
+INSERT INTO matchs (winner, looser, score_winner, score_looser) VALUES (3, 1, 80, 99);
+INSERT INTO matchs (winner, looser, score_winner, score_looser) VALUES (1, 3, 30, 99);
